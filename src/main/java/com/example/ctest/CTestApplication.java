fe730c662e7296e30dda68306b8338bdd42f39ba
lenovo <1209722674@qq.com>
2021-06-04 16:23:36 +0800

    初始化项目

---
+++ src/main/java/com/example/ctest/CTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CTestApplication.class, args);
+    }
+
+}
